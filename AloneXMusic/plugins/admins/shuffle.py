45a1d944f872ad956d1efa062a06ba716a1b5623
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-13 09:07:18 +0300

    Update shuffle.py

--- AloneXMusic/plugins/admins/shuffle.py
+++ AloneXMusic/plugins/admins/shuffle.py
@@ -3,15 +3,21 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
+from config import BANNED_USERS
+from strings import get_command
 from AloneXMusic import app
 from AloneXMusic.misc import db
 from AloneXMusic.utils.decorators import AdminRightsCheck
-from AloneXMusic.utils.inline import close_markup
-from config import BANNED_USERS
+
+# Commands
+SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
 
 
 @app.on_message(
-    filters.command(["shuffle", "cshuffle"]) & filters.group & ~BANNED_USERS
+    filters.command(SHUFFLE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
