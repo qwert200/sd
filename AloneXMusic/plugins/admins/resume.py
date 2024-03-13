788988623dd9b3f2a3e0bb0dc97f832c9df7b6b8
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-13 09:02:25 +0300

    Update resume.py

--- AloneXMusic/plugins/admins/resume.py
+++ AloneXMusic/plugins/admins/resume.py
@@ -1,15 +1,24 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
+from config import BANNED_USERS
+from strings import get_command
 from AloneXMusic import app
 from AloneXMusic.core.call import Alone
 from AloneXMusic.utils.database import is_music_playing, music_on
 from AloneXMusic.utils.decorators import AdminRightsCheck
-from AloneXMusic.utils.inline import close_markup
-from config import BANNED_USERS
+from AloneXMusic.utils.inline.play import close_keyboard
+
+# Commands
+RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
-@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(RESUME_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
