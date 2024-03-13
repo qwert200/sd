1784ef642089f01a943f6b8ab5c131c76f8d9958
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-13 08:45:21 +0300

    Update pause.py

--- AloneXMusic/plugins/admins/pause.py
+++ AloneXMusic/plugins/admins/pause.py
@@ -1,15 +1,24 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
+from config import BANNED_USERS
+from strings import get_command
 from AloneXMusic import app
 from AloneXMusic.core.call import Alone
 from AloneXMusic.utils.database import is_music_playing, music_off
 from AloneXMusic.utils.decorators import AdminRightsCheck
-from AloneXMusic.utils.inline import close_markup
-from config import BANNED_USERS
+from AloneXMusic.utils.inline.play import close_keyboard
+
+# Commands
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(PAUSE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
